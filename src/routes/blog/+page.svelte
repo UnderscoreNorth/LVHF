5b7ede70944196fc0776a834a94db0f7e1d50d43
ALEX-PC\Alex <alex_nguyen@live.ca>
2023-01-26 22:26:50 -0500

    supabase for blog as well.

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,6 +1,12 @@
-First time recording today. Forgot to bring earbuds so we had no clickbeat or metronome to make sure
-our takes were at the same tempo. Didn't figure out how to record the vocals after, guess we'll do
-em at home. -2023-01-23
-<hr />
-Great idea to write this site from scratch at 4am on a Monday instead of using linktree or something,
-but I'm a web developer for a reason. -2023-01-23
+<script>
+	export let data;
+</script>
+
+{#await data}
+	Loading...
+{:then}
+	{#each data.data as blog}
+		{blog.content} -{blog.date}
+		<hr />
+	{/each}
+{/await}
