27c4360dd8b2fbde4182811038547da65d3bdf67
ALEX-PC\Alex <alex_nguyen@live.ca>
2024-07-20 01:27:53 -0400

    Will's playlist

--- src/routes/etc/+page.svelte
+++ src/routes/etc/+page.svelte
@@ -23,6 +23,16 @@
 		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
 		allowfullscreen
 	/><br />
+	Will's playlist of good songs
+	<iframe
+		width="560"
+		height="315"
+		src="https://www.youtube.com/embed/videoseries?list=PL669MP37IVy4Ws-XdQLUWyW2KQleoaHC7"
+		title="YouTube video player"
+		frameborder="0"
+		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+		allowfullscreen
+	/><br />
 </div>
 
 <style>
